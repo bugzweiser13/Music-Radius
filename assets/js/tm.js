231ca158d09c4cf903147001fed4d3f7a114815e
bugzweiser <bugzweiser@gmail.com>
2019-06-08 02:58:32 -0700

    ticket master data retrival / google map start

--- assets/js/tm.js
+++ assets/js/tm.js
@@ -1,91 +1,121 @@
-var limit = 20;
+$(document).ready(function() {
 
-$("#submission").on('click', function() {
-    var genreInput = $("#genre").val();
-    $.ajax({
-        type: "GET",
-        //global url
-        //url: "https://app.ticketmaster.com/discovery/v2/events.json?size=" + limit + "&apikey=Gtk77jcaAuFCC19bpqEWrINnFUHvix20&genreId=" + genreInput + "",
+    // //Initialize Firebase
+    // var config = {
+    //     apiKey: "AIzaSyBFViyub_erkStzv7HIeYmC7YP1LeisBOc",
+    //     authDomain: "music-near-me2.firebaseapp.com",
+    //     databaseURL: "https://music-near-me2.firebaseio.com/",
+    //     storageBucket: "gs://music-near-me2.appspot.com"
+    // };
 
-        //la area link
-        //url: "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=Gtk77jcaAuFCC19bpqEWrINnFUHvix20&classificationName=" + genreInput + "",
+    // firebase.initializeApp(config);
 
-        //US link
-        url: "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=Gtk77jcaAuFCC19bpqEWrINnFUHvix20&classificationName=" + genreInput + "",
-        dataType: "json",
-        success: function(json) {
-            console.log(json);
-            console.log("Genre Requested: " + genreInput);
-            console.log(" ");
+    // var database = firebase.database();
+    // console.log(database);
 
-            for (i = 0; i < limit; i++) {
-                //main search variable
-                //var search = (json._embedded.events[i]);
 
-                //date formats
-                var showDateRtn = moment(showDate).format("MM/DD/YYYY");
-                var saleDateRtn = moment.utc(saleDate).local().format("MM/DD/YYYY"); // @ HH:MM");
+    //Google Map
+    const myApiKey = `AIzaSyBm4jd3w_aMfh42lvqGRGRdWOM9vDf0bYs`;
+    const lat = 34.212933;
+    const lng = -116.3931546;
+    const zoom = 6;
 
-                //search variables
-                var name = (json._embedded.events[i].name)
-                var showDate = (json._embedded.events[i].dates.start.localDate);
-                var venue = (json._embedded.events[i]._embedded.venues[0].name);
-                var saleDate = (json._embedded.events[i].sales.public.startDateTime);
-                var ticketLink = (json._embedded.events[i]._embedded.attractions[0].url);
+    const parentElement = document.getElementById(`map`); // a <div>
+    const script = document.createElement(`script`);
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${myApiKey}`;
+    script.async = true;
+    script.defer = true;
+    script.onload = function() {
+        new google.maps.Map(parentElement, {
+            center: { lat, lng },
+            zoom
+        });
+    };
+    parentElement.insertBefore(script, null);
 
-                // var ticketUrl = ("<a href='" + ticketLink + "' target='_blank'>" + ticketLink + "</a>");
-                var ticketUrl = `<a href=${ticketLink} target='_blank'>${ticketLink}</a>`;
+    //globals
+    var searchLimit = 20;
 
-                var myObj = {
-                    bob: [{
-                        event: 'something',
-                        time: '800'
-                    }]
+    //event search submission
+    $("#submission").on('click', function() {
+
+        //table clear with new search
+        $("#event_info").empty();
+
+        //search input
+        var genreInput = $("#genre").val().trim();
+
+        //data call
+        $.ajax({
+            type: "GET",
+            //global url
+            //url: "https://app.ticketmaster.com/discovery/v2/events.json?size=" + limit + "&apikey=Gtk77jcaAuFCC19bpqEWrINnFUHvix20&genreId=" + genreInput + "",
+
+            //la area link
+            //url: "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=Gtk77jcaAuFCC19bpqEWrINnFUHvix20&classificationName=" + genreInput + "",
+
+            //US link
+            url: "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=Gtk77jcaAuFCC19bpqEWrINnFUHvix20&classificationName=" + genreInput + "",
+            dataType: "json",
+            success: function(ticketMaster) {
+                console.log(ticketMaster);
+                //console.log("Genre Requested: " + genreInput);
+                //console.log(" ");
+
+                for (i = 0; i < searchLimit; i++) {
+                    //main search variable
+                    //var search = (json._embedded.events[i]);
+
+                    //date formats
+                    var showDateRtn = moment(showDate).format("MM/DD/YYYY");
+                    var saleDateRtn = moment.utc(saleDate).local().format("MM/DD/YYYY"); // @ HH:MM");
+
+                    //tm data search variables
+                    var name = (ticketMaster._embedded.events[i].name)
+                    var showDate = (ticketMaster._embedded.events[i].dates.start.localDate);
+                    var venue = (ticketMaster._embedded.events[i]._embedded.venues[0].name);
+                    var saleDate = (ticketMaster._embedded.events[i].sales.public.startDateTime);
+                    //var ticketLink = (ticketMaster._embedded.events[i]._embedded.attractions[0].url); //artist tickets link
+                    var ticketLink = (ticketMaster._embedded.events[i].url) //direct event ticket link
+                        //ticket link population
+                    var ticketUrl = `<a href=${ticketLink} target='_blank'>${ticketLink}</a>`;
+                    var postalCode = (ticketMaster._embedded.events[i]._embedded.venues[0].postalCode);
+                    var localLat = (ticketMaster._embedded.events[i]._embedded.venues[0].location.latitude);
+                    var localLng = (ticketMaster._embedded.events[i]._embedded.venues[0].location.longitude);
+
+                    //table population alternative
+                    // $("#event_info").append("<tr><td> " + name + " </td><td> " + showDateRtn + " </td><td> " + venue + " </td><td>" + saleDateRtn + " </td><td class='click'><a href='" + ticketLink + "' target='_blank'>" + ticketLink + "</a></td><tr>");
+
+                    //table population
+                    var row = $("<tr>");
+                    var tName = $("<td>").append(name);
+                    var tShowDate = $("<td>").append(showDateRtn);
+                    var tVenue = $("<td>").append(venue);
+                    var tSaleDate = $("<td>").append(saleDateRtn);
+                    var tLink = $("<td>").append(ticketUrl);
+                    row.append(tName, tShowDate, tVenue, tSaleDate, tLink);
+                    $("#event_info").append(row);
+
+                    //console log / debugging
+                    //console.log(search);
+                    console.log("------start of info #" + [i] + "------");
+                    //console.log("Artist: " + name);
+                    //console.log("Event Date: " + showDateRtn);
+                    //console.log("Venue: " + venue);
+                    //console.log("Public Sale Date: " + saleDateRtn);
+                    //console.log("Tickets: " + ticketLink);
+                    //console.log("Event Postal Code: " + postalCode);
+                    console.log("Event longitude: " + localLng);
+                    console.log("Event latitude: " + localLat);
+                    console.log("-------end of info #" + [i] + "--------");
+                    //console.log(" ");
+
                 }
-
-                console.log(myObj.bob[0].time);
-
-
-
-                //table population"
-                // $("#event_info").append("<tr><td> " + name + " </td><td> " + showDateRtn + " </td><td> " + venue + " </td><td>" + saleDateRtn + " </td><td class='click'><a href='" + ticketLink + "' target='_blank'>" + ticketLink + "</a></td><tr>");
-
-                var row = $("<tr>");
-                var tName = $("<td>").append(name);
-                var tShowDate = $("<td>").append(showDateRtn);
-                var tVenue = $("<td>").append(venue);
-                var tSaleDate = $("<td>").append(saleDateRtn);
-                var tLink = $("<td>").append(ticketUrl);
-                row.append(tName, tShowDate, tVenue, tSaleDate, tLink);
-                $("#event_info").append(row);
-
-                //$("#event_info").append("<td>");
-                //$("#event_info").append(name);
-                //$("#event_info").append("<td>");
-                //$("#event_info").append(showDateRtn);
-                //$("#event_info").append("<td>");
-                //$("#event_info").append(venue);
-                // $("#event_info").append("<td>");
-                //$("#evnt_info").append(saleDateRtn);
-                //$("#event_info").append("<td>");
-                //$("#event_info").append("<a href='" + ticketLink + "' target='_blank'>" + ticketLink + "</a>");
-
-                //console log / debugging
-                //console.log(search);
-                console.log("------start of info #" + [i] + "------");
-                console.log("Artist: " + name);
-                console.log("Event Date: " + showDateRtn);
-                console.log("Venue: " + venue);
-                console.log("Public Sale Date: " + saleDateRtn);
-                console.log("Tickets: " + ticketLink);
-                console.log("-------end of info #" + [i] + "--------");
-                console.log(" ");
-
+            },
+            error: function(xhr, status, err) {
+                // This time, we do not end up here!
             }
-        },
-        error: function(xhr, status, err) {
-            // This time, we do not end up here!
-        }
+        });
+        return false;
     });
-    return false;
 });
