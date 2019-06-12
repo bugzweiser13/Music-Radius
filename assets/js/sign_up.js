3f1dec66b9c32b24c051d9449a0a7476fd16039d
ChrisA <cteg516@gmail.com>
2019-06-11 21:33:47 -0800

    v0.4

--- assets/js/sign_up.js
+++ assets/js/sign_up.js
@@ -25,6 +25,13 @@ var genere3 = $('#genre3').val().trim();
 
 console.log(email);
 
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+});
+
 firebase.auth().onAuthStateChanged(function(user) {
     window.user = user;
 });
